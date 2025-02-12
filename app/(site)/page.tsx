a255a642d73f551049f69a078a62b385750c37b2
victor <victorpark@sponovation.com>
2025-02-13 00:14:09 +0900

    feat: header 반응형

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,7 +3,13 @@ import React from 'react'
 
 const page = async () => {
   return (
-    <div>home</div>
+      <div className={"min-h-[600px]"}>
+          home
+          <div className="h-[500px] bg-amber-400">우하하</div>
+          <div className="h-[500px] bg-amber-400">우하하</div>
+          <div className="h-[500px] bg-amber-400">우하하</div>
+          <div className="h-[500px] bg-amber-400">우하하</div>
+      </div>
   )
 }
 
--- components/elements/Logo.jsx
+++ components/elements/Logo.jsx
@@ -1,30 +1,41 @@
 'use client'
 import Image from 'next/image'
-import { useRouter } from 'next/navigation';
+import {useRouter} from 'next/navigation';
 import React from 'react'
-import { RxHamburgerMenu } from "react-icons/rx";
+import {RxHamburgerMenu} from "react-icons/rx";
 import IconButton from "./IconButton"
+import {IoCloseOutline} from "react-icons/io5";
 
 
-const Logo = () => {
-  const {push} = useRouter()
-  const onClickLogo = ()=>{
-    push("/")
-  }
-  const onClickMenu = ()=>{
-    push("/")
-  }
-  return (
-    <section className='flex flex-row items-center gap-3'>
-      <IconButton
-        icon={<RxHamburgerMenu size={24} />}
-        onClickIcon={onClickMenu}
-      />
-      <div className='cursor-pointer' onClick={onClickLogo}>
-        <Image alt="logo" width={100} height={30} src={"/main-logo.svg"} />
-      </div>
-    </section>
-  )
+const Logo = ({
+                  isInDrawer = false, onClickClose = () => {
+    }
+              }) => {
+    const {push} = useRouter()
+    const onClickLogo = () => {
+        push("/")
+    }
+    const onClickMenu = () => {
+        push("/")
+    }
+    return (
+        <section className='flex flex-row items-center gap-3'>
+            {isInDrawer ? (
+                <IconButton
+                    icon={<IoCloseOutline size={30} />}
+                    onClickIcon={onClickClose}
+                />
+            ) : (
+                <IconButton
+                    icon={<RxHamburgerMenu size={24} />}
+                    onClickIcon={onClickMenu}
+                />
+            )}
+            <div className='cursor-pointer' onClick={onClickLogo}>
+                <Image alt="logo" width={100} height={30} src={"/main-logo.svg"} />
+            </div>
+        </section>
+    )
 }
 
 export default Logo
